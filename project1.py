0ea228b7f6b4827718bbd081d58cfe6894bf8238
Reyes Parra <breyes@umich.edu>
2025-10-13 19:55:03 -0400

    Update: V3 adding indentions and updating structure

--- project1.py
+++ project1.py
@@ -6,24 +6,23 @@
 
 
 import csv 
-import unittest 
 
 def csv_to_dict_list(self):
 
 # Empty list to have the data
     data_list = []
-# Step 1: I'm opening the file. 
+# Step 1: I'm opening the file. / Update Indentions
     with open('SampleSuperstore.csv' 'r') as csvfile:
-    sample_superstores_reader = csv.reader(csvfile)
+        superstores_reader = csv.DictReader(csvfile)
 
-#Step 2: Loop to get info from each row in the csv 
+#Step 2: Loop to get info from each row in the csv / Update indentions 
 
-        for row in sample_superstores_reader:
+        for row in superstores_reader:
             data_list.append(row) #Step 3: this would add the row to the list we created
 
-# Step 4: we return the function 
+# Step 4: we return the function / update indentions
 
-    return data_list 
+        return data_list 
 
 def calcualte_total_profit_by_category(data_list):
 
@@ -31,3 +30,4 @@ def calcualte_total_profit_by_category(data_list):
 #Item = will be furniture which is a str
 #Sales = will be the integer of the number of the sales 
 #if statement if the item got sold
+
